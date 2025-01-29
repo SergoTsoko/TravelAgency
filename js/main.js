87265d84523a217c9d3abc621058da1c02dce520
SergoTsoko <cokolenko.sergo@gmail.com>
2025-01-29 16:18:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+    },
+  });
